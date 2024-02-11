3d9c57de772b1c23efc1f73ca26496f65d59b0b3
Dave <davit.hovhannisyan2009@gmail.com>
2024-02-11 10:04:46 +0400

    Editing and deleting tasks

--- app/src/main/java/com/example/goalgalaxy/MainActivity.java
+++ app/src/main/java/com/example/goalgalaxy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.goalgalaxy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,39 +19,40 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
+
+    private DatabaseHandler db;
 
     private RecyclerView tasksRecyclerView;
     private ToDoAdapter tasksAdapter;
     private FloatingActionButton fab;
 
     private List<ToDoModel> taskList;
-    private DatabaseHandler db;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Убрана строка, которая скрывает ActionBar
-        // getSupportActionBar().hide();
-
         db = new DatabaseHandler(this);
         db.openDatabase();
 
-        taskList = new ArrayList<>();
-
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        tasksAdapter = new ToDoAdapter(db, this);
+        tasksAdapter = new ToDoAdapter(db,MainActivity.this);
         tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ItemTouchHelper itemTouchHelper = new
+                ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
 
         fab = findViewById(R.id.fab);
 
-        taskList = db.GetAllTasks();
+        taskList = db.getAllTasks();
         Collections.reverse(taskList);
+
         tasksAdapter.setTasks(taskList);
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
 
     @Override
     public void handleDialogClose(DialogInterface dialog){
-        taskList = db.GetAllTasks();
+        taskList = db.getAllTasks();
         Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
         tasksAdapter.notifyDataSetChanged();
--- app/src/main/java/com/example/goalgalaxy/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/goalgalaxy/Utils/DatabaseHandler.java
@@ -21,27 +21,29 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String ID = "id";
     private static final String TASK = "task";
     private static final String STATUS = "status";
-    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + "TEXT, " + STATUS + " INTEGER)";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
+            + STATUS + " INTEGER)";
+
     private SQLiteDatabase db;
 
-    public DatabaseHandler(Context context){
+    public DatabaseHandler(Context context) {
         super(context, NAME, null, VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TODO_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        //Drop the older tables
-        db.execSQL("DROP TABLE IF EXISTS" + TODO_TABLE);
-        //Create tables again
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
+        // Create tables again
         onCreate(db);
     }
 
-    public void openDatabase(){
+    public void openDatabase() {
         db = this.getWritableDatabase();
     }
 
@@ -53,26 +55,28 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     @SuppressLint("Range")
-    public List<ToDoModel> GetAllTasks(){
+    public List<ToDoModel> getAllTasks(){
         List<ToDoModel> taskList = new ArrayList<>();
         Cursor cur = null;
         db.beginTransaction();
         try{
             cur = db.query(TODO_TABLE, null, null, null, null, null, null, null);
-            if (cur != null){
-                if (cur.moveToFirst()){
-                    do {
+            if(cur != null){
+                if(cur.moveToFirst()){
+                    do{
                         ToDoModel task = new ToDoModel();
                         task.setId(cur.getInt(cur.getColumnIndex(ID)));
                         task.setTask(cur.getString(cur.getColumnIndex(TASK)));
                         task.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
                         taskList.add(task);
-                    }while (cur.moveToNext());
+                    }
+                    while(cur.moveToNext());
                 }
             }
         }
         finally {
             db.endTransaction();
+            assert cur != null;
             cur.close();
         }
         return taskList;
@@ -81,16 +85,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void updateStatus(int id, int status){
         ContentValues cv = new ContentValues();
         cv.put(STATUS, status);
-        db.update(TODO_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
-    public void updateTask(int id, String task){
+    public void updateTask(int id, String task) {
         ContentValues cv = new ContentValues();
         cv.put(TASK, task);
-        db.update(TODO_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
     public void deleteTask(int id){
-        db.delete(TODO_TABLE, ID + "=?", new String[] {String.valueOf(id)});
+        db.delete(TODO_TABLE, ID + "= ?", new String[] {String.valueOf(id)});
     }
 }
